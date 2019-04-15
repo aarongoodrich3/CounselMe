1b4bd56dfedbd9aa2f604dbb5e2ea2ee73e7968e
Aaron Goodrich <aarongoodrich3@gmail.com>
2019-04-15 18:59:53 -0400

    setup postgres DB

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,13 @@ ENV PYTHONUNBUFFERED 1
 COPY ./requirements.txt /requirements.txt
 RUN pip install -r /requirements.txt
 
+#Setup DB
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+      gcc libc-dev linux-headers postgresql-dev
+RUN pip install -r /requirements.txt
+RUN apk del .tmp-build-deps
+
 # Setup directory structure
 RUN mkdir /counselMe
 WORKDIR /counselMe
--- counselMe/counselMe/settings.py
+++ counselMe/counselMe/settings.py
@@ -75,11 +75,13 @@ WSGI_APPLICATION = 'counselMe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
